84c2ca5141f4d1828cc7800628486ea61a52d3f0
Tanav <mittaltanav@gmail.com>
2020-08-09 16:02:08 +0530

    created constraint

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,7 +36,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    constrainedLog = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,constrainedLog.body);
 }
 
 function draw(){
@@ -61,4 +63,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+    chain.display();
 }
